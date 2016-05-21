da3ed3dfa7dc6af61d256aea554633c985af75c9
bilel99 <bilel.bekkouche@hotmail.fr>
2016-05-21 12:56:14 +0200

    dd

--- resources/views/front/fo/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('front.layout.home')
-
-
-@section('content')
-
-
-
-@stop
--- resources/views/mail/emails.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>{{Lang::get('mail.emails')}}</h1>
-<hr/>
-
-
-
-<h5>
-    <hr/>
-    <p>{{Lang::get('mail.automate')}}</p>
-</h5>

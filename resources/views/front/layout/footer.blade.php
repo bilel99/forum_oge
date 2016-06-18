2d4ff8c2290890600cb4414335e98deae6f804a0
bilel99 <bilel.bekkouche@hotmail.fr>
2016-06-18 16:49:09 +0200

    maj f and b

---
+++ resources/views/front/layout/footer.blade.php
@@ -0,0 +1,10 @@
+    <footer>
+        <div id="info-bar">
+            <div class="container clearfix">
+                <span class="all-tutorials"><a href="http://bootsnipp.com/cppratikcp">← all tutorials</a></span>
+                <span class="back-to-tutorial"><a href="https://www.facebook.com/pratik.chauhan.cp">CHUAHAN PRATIK</a></span>
+            </div>
+        </div><!-- /#top-bar -->
+    </footer><!-- /footer -->
+
+</div><!-- /#wrapper -->
---
+++ resources/views/front/layout/nav.blade.php
@@ -0,0 +1,12 @@
+<div id="wrapper">
+
+    <header>
+        <div class="container clearfix">
+            <h1 id="logo">
+                Forum Oge
+            </h1>
+            <nav>
+                <a href="">Accueil</a>
+            </nav>
+        </div>
+    </header><!-- /header -->
